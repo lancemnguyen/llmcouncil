3c33630c0821a91b94785865b7d271a5ed4d1044
lance nguyen <lancesudo@gmail.com>
2025-09-02 12:03:30 -0700

    bmac msg

--- src/components/BuyMeACoffee.tsx
+++ src/components/BuyMeACoffee.tsx
@@ -12,7 +12,7 @@ export function BuyMeACoffee() {
           alt="Buy me a coffee"
           className="h-4 w-4 mr-2"
         />
-        Buy me a coffee
+        Donations are appreciated!
       </a>
     </div>
   );
